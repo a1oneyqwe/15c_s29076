aa4aec97e2defcd317e7adde24b9b52cd468c8c2
s29076 <s29076@pjwstk.edu.pl>
2024-03-18 14:33:38 +0100

    second task

--- main.py
+++ main.py
@@ -2,3 +2,8 @@
 squares = [x**2 for x in range(1, 11)]
 print(squares)
 
+# task_2.py
+def generate_squares(start, end):
+    return [x**2 for x in range(start, end)]
+
+print(generate_squares(1, 11))
