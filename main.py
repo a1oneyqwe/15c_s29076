6b1efa6252e0368825ef75d1183a7aa4fc1213b8
s29076 <s29076@pjwstk.edu.pl>
2024-03-18 14:54:11 +0100

    fifth task

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# task_1.py
+# task_1
 squares = [x**2 for x in range(1, 11)]
 print(squares)
 
 
-# task_2.py
+# task_2
 def generate_squares(start, end):
     return [x**2 for x in range(start, end)]
 
 print(generate_squares(1, 11))
 
 
-# task_3.py
+# task_3
 class SquareGenerator:
     def generate_squares(self, start, end):
         return [x**2 for x in range(start, end)]
@@ -18,7 +18,7 @@ class SquareGenerator:
 square_gen = SquareGenerator()
 print(square_gen.generate_squares(1, 11))
 
-# task_4.py
+# task_4
 import math
 
 class SquareGenerator:
@@ -27,3 +27,19 @@ class SquareGenerator:
 
 square_gen = SquareGenerator()
 print(square_gen.generate_squares(1, 11))
+
+
+# task_5
+class SquareGenerator:
+    def generate_squares(self, start, end):
+        if end < start:
+            raise ValueError("End of range must be greater than or equal to start")
+        return [x**2 for x in range(start, end)]
+
+square_gen = SquareGenerator()
+try:
+    print(square_gen.generate_squares(1, 10))
+except ValueError as e:
+    print(e)
+
+
