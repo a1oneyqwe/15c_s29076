c873024e099d3090651f606bb16e77aae27e4650
s29076 <s29076@pjwstk.edu.pl>
2024-03-18 14:35:30 +0100

    third task

--- main.py
+++ main.py
@@ -2,8 +2,18 @@
 squares = [x**2 for x in range(1, 11)]
 print(squares)
 
+
 # task_2.py
 def generate_squares(start, end):
     return [x**2 for x in range(start, end)]
 
 print(generate_squares(1, 11))
+
+
+# task_3.py
+class SquareGenerator:
+    def generate_squares(self, start, end):
+        return [x**2 for x in range(start, end)]
+
+square_gen = SquareGenerator()
+print(square_gen.generate_squares(1, 11))
